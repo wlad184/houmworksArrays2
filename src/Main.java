3e3653509b23bbfae1c1f6c0ee11782602909120
wlad184 <evel184@yandex.ru>
2022-12-05 21:26:21 +0700

    ДЗ массивы ч.2

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = generateRandomArray();
+
+    }
+        public static int[] generateRandomArray() {
+            java.util.Random random = new java.util.Random();
+            int[] arr = new int[30];
+            for (int i = 0; i < arr.length; i++) {
+                arr[i] = random.nextInt(100_000) + 100_000;
+            }
+            return arr;
+        }
+
+}
